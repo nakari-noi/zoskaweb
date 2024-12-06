ba30f038e5b9bb932ec69a8ec4ce709658650984
nakari-noi <tichyajo@gmail.com>
2024-12-06 09:09:43 +0100

    change in auth

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -13,6 +13,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  debug: true, // Enable/disable detailed logging
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/auth/prihlasenie',
